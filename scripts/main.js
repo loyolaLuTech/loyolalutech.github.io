ec96c33582d7172ece6a2a75708698bcbb365aed
Thomas J Fox <thomasjfox1@gmail.com>
2015-10-21 16:13:38 -0500

    fixed js where there was an issue of div placement due to function execution order

--- scripts/main.js
+++ scripts/main.js
@@ -1,22 +1,13 @@
 // javascript wizardry
 $(function(){
-  $('.logo-container').css('position','absolute');
-  $('.logo-container').css('top', Math.max(0, (($(window).height() - $('.logo-container').outerHeight()) / 2) + $(window).scrollTop()) + "px");
-  $('.logo-container').css('left', Math.max(0, (($(window).width() - $('.logo-container').outerWidth()) / 2) + $(window).scrollLeft()) + "px");
-
-  $(window).resize(function() {
-    $('.logo-container').css('position','absolute');
-    $('.logo-container').css('top', Math.max(0, (($(window).height() - $('.logo-container').outerHeight()) / 2) + $(window).scrollTop()) + "px");
-    $('.logo-container').css('left', Math.max(0, (($(window).width() - $('.logo-container').outerWidth()) / 2) + $(window).scrollLeft()) + "px");
-  });
-});
-
-$(function(){
   var viewportWidth = $(window).width();
   var viewportHeight = $(window).height();
 
   $('body').css('height', viewportHeight + 'px');
   $('body').css('width', viewportWidth + 'px');
+  $('.logo-container').css('position','absolute');
+  $('.logo-container').css('top', Math.max(0, ((viewportHeight - $('.logo-container').outerHeight()) / 2) + $(window).scrollTop()) + "px");
+  $('.logo-container').css('left', Math.max(0, ((viewportWidth - $('.logo-container').outerWidth()) / 2) + $(window).scrollLeft()) + "px");
 
   $(window).resize(function() {
       var viewportWidth = $(window).width();
@@ -24,5 +15,9 @@ $(function(){
 
       $('body').css('height', viewportHeight + 'px');
       $('body').css('width', viewportWidth + 'px');
+      $('.logo-container').css('position','absolute');
+      $('.logo-container').css('top', Math.max(0, ((viewportHeight - $('.logo-container').outerHeight()) / 2) + $(window).scrollTop()) + "px");
+      $('.logo-container').css('left', Math.max(0, ((viewportWidth - $('.logo-container').outerWidth()) / 2) + $(window).scrollLeft()) + "px");
+
   });
 });
